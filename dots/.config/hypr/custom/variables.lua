a0164bd3352770a943f159b890ba1c67c60b5ac6
Mr. Vanta <vanta@vantavoids.com>
2026-07-30 16:41:35 +0200

    custom: use ghostty as terminal (Super+Return)

--- dots/.config/hypr/custom/variables.lua
+++ dots/.config/hypr/custom/variables.lua
@@ -1,1 +1,5 @@
+-- Personal variable overrides (loaded after hyprland/variables.lua)
 
+-- Terminal for Super+Return / Super+T / Ctrl+Alt+T.
+-- ghostty is configured (in ~/.config/ghostty/config) to launch zsh.
+terminal = "ghostty"
